0eed28324ac574bc57e2e0118f884d8d760d246d
Veljko Cvetkovic <99930588+ve001@users.noreply.github.com>
2024-08-14 01:37:19 +0200

    km h

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const displayDataDiv = document.getElementById("DisplayData")
 function success(pos) {
   const crd = pos.coords;
 
-  displayDataDiv.innerHTML = JSON.stringify(pos)
+  //displayDataDiv.innerHTML = JSON.stringify(pos)
 
   displayDataDiv.innerHTML = `Timestamp: ${pos.timestamp}<br><br>
                               Coords<br>
                               Range: ${pos.coords.accuracy.toFixed(2)} meters<br>
                               Latitude: ${pos.coords.latitude}<br>
                               Longtitude ${pos.coords.longitude}<br>
-                              Speed: ${pos.coords.speed}`
+                              Speed: ${pos.coords.speed != null ? pos.coords.speed.toFixed(2) * 3.6 : 0}km/h`
 }
 
 function error(err) {
