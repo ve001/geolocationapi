1a33e7d514325ed0b6b8c1929a8d26f59a3d76dc
Veljko Cvetkovic <99930588+ve001@users.noreply.github.com>
2024-08-14 01:22:12 +0200

    44
    gg

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ function success(pos) {
   const crd = pos.coords;
 
   displayDataDiv.innerHTML = JSON.stringify(pos)
+
+  displayDataDiv.innerHTML = `Timestamp: ${pos.timestamp}<br>
+                              Coords<br>
+                              Range: ${pos.coords.accuracy} meters<br>
+                              Latitude: ${pos.coords.latitude}<br>
+                              Longtitude ${pos.coords.longitude}<br>
+                              Speed: ${pos.coords.speed}`
 }
 
 function error(err) {
